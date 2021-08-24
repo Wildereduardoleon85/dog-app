6e6a3e57a9fa295d7b3d07a6ea4f127ca908d422
Wilder León <wilder_eduardo85@hotmail.com>
2021-08-24 16:09:06 -0400

    carousel updated

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,25 +1,68 @@
-import React, {useEffect, useContext} from 'react';
-import MainContext from '../../context/main/mainContext';
+import React, {useState, useEffect} from 'react';
+import carouselImages from '../../data/carouselImages';
+import {capitalize} from '../../utils/utils';
 import './carousel.scss';
 
 const Carousel = () => {
-    const mainContext = useContext(MainContext);
+    const [images, setImages] = useState(carouselImages);
+    const [index, setIndex] = useState(0);
 
-    const {carouselImages, setCarouselImages} = mainContext
+    useEffect(()=> {
+        if(index < 0){
+            setIndex(images.length -1)
+        }
+        if(index > images.length -1){
+            setIndex(0)
+        }
+    }, [images, index])
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCarouselImages()
-        }, 5000);
-        return () => clearInterval(interval);
-        // eslint-disable-next-line
-      }, []);
+    useEffect(()=> {
+        const slider = setInterval(()=>{
+            handleNext()
+        }, 4000);
+        return ()=> clearInterval(slider)
+    }, [index])
+
+    const handleNext = () => {
+        setIndex(index + 1)
+    }
+
+    const handlePrev = () => {
+        setIndex(index - 1)
+    }
 
     return (
         <div className="carousel">
-            <div>
-                <img src={carouselImages} alt={carouselImages}></img>
+            <button onClick={handlePrev} className="button-primary btn-next" >
+                <i className="fas fa-chevron-left"></i>
+            </button>
+            <div className="main-container">
+                {images.map((item, imageIndex)=> {
+                    let position = 'next'
+                    if(imageIndex === index){
+                        position = 'active'
+                    }
+                    if(imageIndex === index -1 || 
+                        (index === 0 && imageIndex === images.length -1)){
+                        position = 'prev'
+                    }
+                    return(
+                        <div key={item} className={`image-container ${position}`}>
+                            <div className="image">
+                                <img src={`/img/carousel/${item}.jpg`} alt={item}></img>
+                            </div>
+                            {item.split('-')[1] ? (
+                            <h2>{capitalize(item.split('-')[0]) + ' ' + capitalize(item.split('-')[1])}</h2>
+                            ) : (
+                                <h2>{capitalize(item.split('-')[0])}</h2>
+                            )}
+                        </div>
+                    )
+                })}
             </div>
+            <button onClick={handleNext} className="button-primary btn-next" >
+                <i className="fas fa-chevron-right"></i>
+            </button>
         </div>
     )
 }
---
+++ src/data/carouselImages.js
@@ -0,0 +1,26 @@
+const carouselImages = [
+    'akita',
+    'basset-hound',
+    'beagle',
+    'border-collie',
+    'boxer',
+    'bulldog-english',
+    'bullterrier-staffordshire',
+    'cocker-spaniel',
+    'collie',
+    'dalmatian',
+    'fox-terrier',
+    'german-shepherd',
+    'golden-retriever',
+    'great-dane',
+    'husky',
+    'leonberger',
+    'mountain-bernese',
+    'pug',
+    'rottweiler',
+    'saint-bernard',
+    'schnauzer',
+    'sheepdog-english'
+]
+
+export default carouselImages
