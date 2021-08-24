e9bd7116873f0617d89c4a868a653eccb2052f10
Wilder León <wilder_eduardo85@hotmail.com>
2021-08-24 19:03:01 -0400

    responsive details updated

--- src/components/layout/NoResults/NoResults.js
+++ src/components/layout/NoResults/NoResults.js
@@ -4,11 +4,12 @@ import './no-results.scss';
 const NoResults = () => {
     return (
         <div className="no-results">
-            <div>
-                <img src="/img/sadDog.jpg" alt="" />
-                
+            <div className="no-results-container">
+                <div>
+                    <img src="/img/sadDog.jpg" alt="" />
+                </div>
+                <h2>Sorry, there's no images found for the selected Breed...</h2>
             </div>
-            <h2>Sorry, there's no images found for the selected Breed...</h2>
         </div>
     )
 }
